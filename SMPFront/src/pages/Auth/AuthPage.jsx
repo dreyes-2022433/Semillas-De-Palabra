5cf26249071c69feeaa62555476c7e036fa77a42
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-06-11 15:02:52 -0600

    Actualización de AuthPage para rediseño y conexión con Register

--- SMPFront/src/pages/Auth/AuthPage.jsx
+++ SMPFront/src/pages/Auth/AuthPage.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import { Login } from '../../components/Auth/Login.jsx'
+"use client"
 
+import { useState } from "react"
+import { Login } from "../../components/Auth/Login.jsx"
+import { Register } from "../../components/Auth/Register.jsx"
 
 //Componente exportado por Named (Quiero tener más componentes en un archivo)
 export const AuthPage = () => {
-    const [isLogin, setIsLogin] = useState(true)
-    const handleIsLogin = ()=>{
-        setIsLogin((prev)=> !prev)
-    } 
+  const [isLogin, setIsLogin] = useState(true)
+  const handleIsLogin = () => {
+    setIsLogin((prev) => !prev)
+  }
   return (
     <div className="auth-container">
-        {
-            isLogin ? (
-                <>
-        
-                <Login handleIsLogin={handleIsLogin}/>
-                </>
-            ) : (
-                <>    
-                </>
-            )
-        }
+      {isLogin ? (
+        <>
+          <Login handleIsLogin={handleIsLogin} />
+        </>
+      ) : (
+        <>
+          <Register handleIsLogin={handleIsLogin} />
+        </>
+      )}
     </div>
   )
 }
