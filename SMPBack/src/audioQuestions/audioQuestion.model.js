2f1dfc11cc8fc41047baeb77d7fe80b0bffe8e60
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-06-11 22:37:09 -0600

    Se modela audioQuestion

---
+++ SMPBack/src/audioQuestions/audioQuestion.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema, model} from 'mongoose'
+
+const audioQuestionSchema = Schema(
+    {
+        name: {
+            type: String,
+            maxLength: [100, `Can´t be overcome 100 characters`],
+            required: [true, 'Name is required']
+        },
+        audio: {
+            type: String
+        },
+        img1:{
+            type: String
+        },
+        answer1:{
+            type: Boolean
+        },
+        img2:{
+            type: String
+        },
+        answer2:{
+            type: Boolean
+        },
+        img3:{
+            type: String
+        },
+        answer3:{
+            type: Boolean
+        },
+        img4:{
+            type: String,
+        },
+        answer4:{
+            type: Boolean
+        },
+        order:{
+            type: Number,
+            required: [true, 'Order is required']
+        },
+        module: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Module',
+            required: [true, 'Module is required']
+        }
+    }
+)
+
+export default model('AudioQuestion', audioQuestionSchema)
