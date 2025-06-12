0fce100967f1d23aa93b147e07525e7fcc7153a5
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-06-11 22:42:08 -0600

    Se modela videoLesson

---
+++ SMPBack/src/videoLessons/videoLesson.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model } from 'mongoose'
+
+const videoLessonSchema = Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            maxLength: [100, `Can´t be overcome 100 characters`]
+        },
+        video: {
+            type: String
+        },
+        order: {
+            type: Number,
+            required: [true, 'Order is required']
+        },
+        module: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Module',
+            required: [true, 'Order is required']
+        }
+    }
+)
+
+export default model('VideoLesson', videoLessonSchema)
