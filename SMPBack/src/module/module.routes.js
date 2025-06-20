0f67acb02524c383e619ef8fedf7a61aeeb46f49
dreyes-2022433 <dreyes-2022433@kinal.edu.gt>
2025-06-19 22:18:20 -0600

    Se agrega la opción para agregar imagenes al modulo

--- SMPBack/src/module/module.routes.js
+++ SMPBack/src/module/module.routes.js
@@ -18,6 +18,12 @@ import {
     validateJwt 
 } from '../../middlewares/validate.jwt.js'
 
+import upload from '../../middlewares/multer.js'
+import cloudinary from '../../config/cloudinary.js'
+import Module from './module.model.js'
+
+
+
 const api = Router()
 
 api.post(
@@ -53,4 +59,46 @@ api.put(
 )
 
 api.post('/getModule', getModule )
+
+api.post('/uploadModule', upload.single('image'), async (req, res) => {
+    try{
+        const {name, description} = req.body
+        const stream = cloudinary.uploader.upload_stream({
+            folder: 'SemillasDePalabra'
+        },
+    async (error, result) => {
+        if (error) {
+            return res.status(500).send({
+                message: 'Error uploading image',
+                error
+            })
+        }
+
+        const module = new Module({
+            name,
+            description,
+            img: result.secure_url
+        })
+
+        await module.save()
+
+        return res.status(200).send({
+            message: 'Module image uploaded successfully',
+            module
+        })
+    })
+    stream.end(req.file.buffer)
+    }catch (err) {
+        console.error(err)
+        return res.status(500).send({
+            message: 'Error uploading module image',
+            err
+        })
+    }
+
+
+})
+
+
+
 export default api
