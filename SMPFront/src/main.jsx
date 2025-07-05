6498c943fc4e382af67cda1e789d09281bf869ee
dreyes-2022433 <dreyes-2022433@kinal.edu.gt>
2025-07-05 02:34:35 -0600

    Se agrega contexto a la pagina

--- SMPFront/src/main.jsx
+++ SMPFront/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import {UserModulesProvider} from '../src/pages/MainPages/userModuleContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <UserModulesProvider>
+          <App />
+        </UserModulesProvider>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
